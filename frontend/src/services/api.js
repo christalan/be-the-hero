4887f84db7cd292f7bbecb93867a626a925d0893
Alan C <alancsystem@gmail.com>
2020-09-14 14:08:06 +0000

    atualiza dependencias

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'https://3333-dd3575f4-176d-4e9c-975d-e440076dd992.ws-us02.gitpod.io' //'http://localhost:3333'
+	baseURL: 'https://3333-d1344573-c9fb-4497-a718-d23d0b8c7e47.ws-us02.gitpod.io' //'http://localhost:3333'
 })
 
 export default api;
